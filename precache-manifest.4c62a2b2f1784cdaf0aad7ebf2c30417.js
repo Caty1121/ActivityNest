5959b9d2ab58d6d1ef97b2447151f455fdaa8725
Caty <catymaepaints@gmail.com>
2024-06-16 20:17:55 -0400

    Updates

---
+++ precache-manifest.4c62a2b2f1784cdaf0aad7ebf2c30417.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408e4a2343d49a18ad6064ffc4544e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f649b27a1cb541d97cb0",
+    "url": "/static/css/main.3002aee2.chunk.css"
+  },
+  {
+    "revision": "9d2927c14b4ab249b24e",
+    "url": "/static/js/2.45cc7e97.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "f649b27a1cb541d97cb0",
+    "url": "/static/js/main.9c324e3e.chunk.js"
+  },
+  {
+    "revision": "dddac0f3335687289ff3",
+    "url": "/static/js/runtime~main.4a9a7a8f.js"
+  },
+  {
+    "revision": "b140bc0053827a8651f7ea27fe4915c7",
+    "url": "/static/media/crafts2.b140bc00.jpg"
+  },
+  {
+    "revision": "1d5c3ab1043e218fad7eb62b508c581d",
+    "url": "/static/media/family1.1d5c3ab1.jpg"
+  },
+  {
+    "revision": "d1ca6aa41fc92d19fd8f9ce215f70478",
+    "url": "/static/media/family2.d1ca6aa4.jpg"
+  },
+  {
+    "revision": "30f1f48defd48fd61514e6033dd4f5cc",
+    "url": "/static/media/logo.30f1f48d.png"
+  },
+  {
+    "revision": "38e544fbbb36b278cce7be871d45e68e",
+    "url": "/static/media/outdoor-activities1.38e544fb.jpg"
+  },
+  {
+    "revision": "763faa9398876f7a6756393d9a055485",
+    "url": "/static/media/outdoor-activities2.763faa93.jpg"
+  },
+  {
+    "revision": "297771ee83a6dd218430887725c83940",
+    "url": "/static/media/outdoor-activities3.297771ee.jpg"
+  },
+  {
+    "revision": "ceb505a6474021df20f87964809c784b",
+    "url": "/static/media/outdoor-activities4.ceb505a6.jpg"
+  },
+  {
+    "revision": "374392b973b7fba08bc922f08e0ae196",
+    "url": "/static/media/seasonal-winter.374392b9.jpg"
+  },
+  {
+    "revision": "1f804a5ff3ea17ee9c35078db6fe8b91",
+    "url": "/static/media/tree-tops.1f804a5f.jpg"
+  }
+]);
